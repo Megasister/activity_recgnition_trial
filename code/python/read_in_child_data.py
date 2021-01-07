4b857675dc392c555e726523189a49907d654189
Megasister <44200118+Megasister@users.noreply.github.com>
2021-01-07 10:24:30 +0800

    Update read_in_child_data.py

--- code/python/read_in_child_data.py
+++ code/python/read_in_child_data.py
@@ -2,31 +2,33 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import scipy.io
 from feature_extraction.tsfeature import feature_core, feature_fft, feature_time
-
-link1 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__10-00-29.csv'
-link2 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__11-14-14.csv'
-link3 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__14-35-58.csv'
-
-lines = pd.read_csv(link3)
-# print(lines)
-
-lines.columns = ['ord', 'time', 'ax', 'ay', 'az', 'wx', 'wy', 'wz', 'tile']
-lines.drop(['ord', 'time', 'tile'], axis=1, inplace=True)
-
-container_A1, container_G1 = [], []
-a_axis = ['ax', 'ay', 'az']
-g_axis = ['wx', 'wy', 'wz']
-for axis in a_axis:
-    container_A1.append(pd.Series(lines[axis], name=axis))
-for axis in g_axis:
-    container_G1.append(pd.Series(lines[axis], name=axis))
+import parameter
 
 
-container_A1 = np.array(container_A1)
-container_A1 = container_A1.T
-container_G1 = np.array(container_G1)
-container_G1 = container_G1.T
+def get_container(link):
+    lines = pd.read_csv(link)
+    # print(lines)
+
+    lines.columns = ['ord', 'time', 'ax', 'ay', 'az', 'wx', 'wy', 'wz', 'tile']
+    lines.drop(['ord', 'time', 'tile'], axis=1, inplace=True)
+
+    container_A1, container_G1 = [], []
+    a_axis = ['ax', 'ay', 'az']
+    g_axis = ['wx', 'wy', 'wz']
+    for axis in a_axis:
+        container_A1.append(pd.Series(lines[axis], name=axis))
+    for axis in g_axis:
+        container_G1.append(pd.Series(lines[axis], name=axis))
+
+
+    container_A1 = np.array(container_A1)
+    container_A1 = container_A1.T
+    container_G1 = np.array(container_G1)
+    container_G1 = container_G1.T
+
+    return container_A1, container_G1
 
 
 # label the slidding window, return tag, start by a boundary closer to target activity, include more 'others' sample
@@ -34,25 +36,44 @@ def label_target_activity(stamp, window_size, overlap_tolerance, head_tail_exten
     # initialize overall start and end stamp with head_tail_extend
     start, end, taglist = [], [], []
     for extend in head_tail_extend:
-        for i, (_, act) in enumerate(stamp.items()):
-            start.append(0 if act[0] - extend < 0 else act[0] - extend)
-            end.append(act[1] + extend)
+        s_bucket, e_bucket = [],[]
+        for _, (_, act) in enumerate(stamp.items()):
+            for i, tup in enumerate(act):
+                if i == 0:
+                    s_bucket.append(0 if tup[0] - extend < 0 else tup[0] - extend)
+                if i == len(act)-1:
+                    e_bucket.append(tup[1] + extend)
+        start.append(s_bucket)
+        end.append(e_bucket)
+    print(start)
+    print(end)
     # tag corresponding activity for sliding window
     # that has an overlap with activity larger than overlap_tolerance = frequency * react_time
     # and other position 'others'
-    for k in range(len(window_size)):
+
+    # k control the window size
+    for k in range(len(start)):
+        # j control activity_chunk boundary
         tags = {}
-        for j in range(len(start)):
-            for i in range(start[j], end[j], window_size[k]//2):
+        for j in range(len(start[k])):
+            # i control slide window position
+            for i in range(start[k][j], end[k][j], window_size[k]//2):
                 tags[i], s, e = 'others', i, i + window_size[k]
                 for name, act in stamp.items():
-                    if (e - overlap_tolerance[k] < act[0]) or (s + overlap_tolerance[k] > act[1]):
-                        continue
-                    else:
-                        if name.startswith('r'):
-                            tags[i] = 'rope_skipping'
+                    for tup in act:
+                        if (e - overlap_tolerance[k] < tup[0]) or (s + overlap_tolerance[k] > tup[1]):
+                            continue
                         else:
-                            tags[i] = name
+                            if name.startswith('r'):
+                                tags[i] = 'rope_skipping'
+                            if name.startswith('w'):
+                                tags[i] = 'walking'
+                            if name.startswith('s'):
+                                tags[i] = 'sit_up1'
+                            if name.startswith('j'):
+                                tags[i] = 'jog'
+                            break
+        print(tags)
         taglist.append(tags)
     return taglist
 
@@ -100,29 +121,21 @@ def get_aw_feature(walk_start_stamp, walk_end_stamp, container_A1, container_G1)
 
 
 def process():
+    container_A1, container_G1 = get_container(parameter.kid_link5)
     stamp = {}
-    # s1, e1, s2, e2 = 56750, 65740, 119700, 123500
-    # stamp['rope_skipping'] = [s2, e2]
-    # stamp['sit_up1'] = [s1, e1]
+    print(container_A1)
 
-    # s1, e1, s2, e2, s3, e3, s4, e4 = 17580, 18740, 38220, 40610, 41370, 42150, 42650, 43130
-    # s5, e5 = 112100, 114300
-    # stamp['rope_skipping1'] = [s1, e1]
-    # stamp['rope_skipping2'] = [s2, e2]
-    # stamp['rope_skipping3'] = [s3, e3]
-    # stamp['rope_skipping4'] = [s4, e4]
-    # stamp['sit_up1'] = [s5, e5]
-
-    s1, e1, s2, e2 = 70750, 72960, 111800, 116500
-    stamp['rope_skipping'] = [s2, e2]
-    stamp['sit_up1'] = [s1, e1]
+    # kid_link5
+    s1, e1, s2, e2= 609, 37730, 52600, 78370
+    stamp['walking'] = [(s1, e1)]
+    stamp['jog'] = [(s2, e2)]
 
     # 调试绘图
     # sos = signal.butter(3, 1, 'lp', fs=52, output='sos')
     # accdata = signal.sosfilt(sos, [g[-1] for g in container_G1])
     # plt.plot([sumvector(a[1], a[2], a[3]) for a in container_A1], label='sum acceleration')
-    # plt.plot([g[2] for g in container_G1], label='z angular velocity')
-    # # plt.plot(accdata, label='high pass filter wave')
+    # plt.plot([g[2] for g in container_G1[s1-ext:e3+ext]], label='z angular velocity')
+    # plt.plot(accdata, label='high pass filter wave')
     # plt.legend()
     # plt.plot([a[1] for a in container_G1], label='wx')
     # plt.plot([a[2] for a in container_G1], label='wy')
@@ -131,7 +144,7 @@ def process():
     # plt.plot([d[1] for d in delta_angle], label='fai')
     # plt.plot([d[2] for d in delta_angle], label='psi')
     # plt.show()
-
+    i
     # window_time = [0.5,1,3,5,8,10]  # s time of a window frame
     window_time = [10]
     print('window time is:', window_time)
@@ -164,9 +177,9 @@ def process():
     print('feature num is:', len(next(iter(ftlist[0].values()))[0]))
     print('total dim is:', len(next(iter(ftlist[0].values()))))
     # save the feature and tags in pickel file
-    pd.DataFrame(ftlist).to_pickle(r'.\multiclass_new_tag\feature_child3_w10_norm.pickle')
-    pd.DataFrame(taglist).to_pickle(r'.\multiclass_new_tag\tag_child3_w10_norm.pickle')
-    print('preprocess over')
+    # pd.DataFrame(ftlist).to_pickle(r'.\multiclass_new_tag\feature_child1_w10_test.pickle')
+    # pd.DataFrame(taglist).to_pickle(r'.\multiclass_new_tag\tag_child1_w10_test.pickle')
+    # print('preprocess over')
 
 
 if __name__ == '__main__':
