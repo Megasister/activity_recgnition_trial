87bc3464edf8c91dd0a7a5465779e6653e4cb287
Megasister <44200118+Megasister@users.noreply.github.com>
2021-01-07 10:25:28 +0800

    Update __init__.py

--- code/python/parameter/__init__.py
+++ code/python/parameter/__init__.py
@@ -1,3 +1,10 @@
 link1 = r"C:\Users\20804370\Desktop\202012081708-ancfthrveijdbckh\xtcdata\Logs_Collector\Folder_20180223\Log.main_sys_2018-02-23__00-01-46.log"
 link2 = r"C:\Users\20804370\Desktop\202012081708-ancfthrveijdbckh\xtcdata\Logs_Collector\Folder_20180223\Log.main_sys_2018-02-23__08-11-42.log"
 link_sk = r"C:\Users\20804370\Desktop\202012211553-ancfthrveijdbckh\xtcdata\Logs_Collector\Folder_20201221\Log.main_sys_2020-12-21__10-49-32.log"
+
+kid_link1 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__10-00-29.csv'
+kid_link2 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__11-14-14.csv'
+
+kid_link3 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__14-35-58.csv'
+kid_link4 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__12-57-13.csv'
+kid_link5 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__15-48-08.csv'
