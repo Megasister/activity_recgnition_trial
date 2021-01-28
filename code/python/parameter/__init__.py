b812921a1db7db03da719aba6dd2177d050ee61d
Megasister <44200118+Megasister@users.noreply.github.com>
2021-01-28 11:08:06 +0800

    Update __init__.py

--- code/python/parameter/__init__.py
+++ code/python/parameter/__init__.py
@@ -1,10 +1,87 @@
 link1 = r"C:\Users\20804370\Desktop\202012081708-ancfthrveijdbckh\xtcdata\Logs_Collector\Folder_20180223\Log.main_sys_2018-02-23__00-01-46.log"
-link2 = r"C:\Users\20804370\Desktop\202012081708-ancfthrveijdbckh\xtcdata\Logs_Collector\Folder_20180223\Log.main_sys_2018-02-23__08-11-42.log"
 link_sk = r"C:\Users\20804370\Desktop\202012211553-ancfthrveijdbckh\xtcdata\Logs_Collector\Folder_20201221\Log.main_sys_2020-12-21__10-49-32.log"
 
 kid_link1 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__10-00-29.csv'
 kid_link2 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__11-14-14.csv'
-
 kid_link3 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__14-35-58.csv'
 kid_link4 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__12-57-13.csv'
 kid_link5 = r'C:\Users\20804370\Desktop\activity-rec-feature-extr\activityrecognition\code\python\child_data\motion_data_2020-12-26__15-48-08.csv'
+
+kid2_link1 = r'C:\Users\20804370\Desktop\log_main_data\raw\Log.main_sys_2021-01-16__10-45-20.log'
+kid2_link2 = r'C:\Users\20804370\Desktop\log_main_data\raw\Log.main_sys_2021-01-16__11-48-17.log'
+kid2_link3 = r'C:\Users\20804370\Desktop\log_main_data\raw\Log.main_sys_2021-01-16__12-55-57.log'
+kid2_link4 = r'C:\Users\20804370\Desktop\log_main_data\raw\Log.main_sys_2021-01-16__13-37-18.log'
+
+
+
+# people = 'girl1'
+# time1 = datetime.strptime('10:59:51.000000', '%H:%M:%S.%f').time()
+# time2 = datetime.strptime('11:00:35.000000', '%H:%M:%S.%f').time()
+# tag['boy2_rpsk'] = (time1, time2)
+#
+# time3 = datetime.strptime('10:52:25.000000', '%H:%M:%S.%f').time()
+# time4 = datetime.strptime('10:53:01.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp1'] = (time3, time4)
+#
+# time5 = datetime.strptime('10:53:12.000000', '%H:%M:%S.%f').time()
+# time6 = datetime.strptime('10:53:37.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp2'] = (time5, time6)
+#
+# time7 = datetime.strptime('10:34:11.000000', '%H:%M:%S.%f').time()
+# time8 = datetime.strptime('10:46:04.000000', '%H:%M:%S.%f').time()
+# tag['boy2_longhaul'] = (time7, time8)
+
+
+# people = 'boy1'
+# # kid2
+# time1 = datetime.strptime('12:40:00.000000', '%H:%M:%S.%f').time()
+# time2 = datetime.strptime('12:40:56.000000', '%H:%M:%S.%f').time()
+# tag['boy2_rpsk'] = (time1, time2)
+#
+# time3 = datetime.strptime('12:34:37.000000', '%H:%M:%S.%f').time()
+# time4 = datetime.strptime('12:35:15.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp1'] = (time3, time4)
+#
+# time5 = datetime.strptime('12:35:34.000000', '%H:%M:%S.%f').time()
+# time6 = datetime.strptime('12:36:06.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp2'] = (time5, time6)
+#
+# time7 = datetime.strptime('11:40:02.000000', '%H:%M:%S.%f').time()
+# time8 = datetime.strptime('11:47:55.000000', '%H:%M:%S.%f').time()
+# tag['boy2_longhaul'] = (time7, time8)
+
+
+# people = 'girl2'
+# time1 = datetime.strptime('13:40:26.000000', '%H:%M:%S.%f').time()
+# time2 = datetime.strptime('13:41:43.000000', '%H:%M:%S.%f').time()
+# tag['boy2_rpsk'] = (time1, time2)
+#
+# time3 = datetime.strptime('13:45:34.000000', '%H:%M:%S.%f').time()
+# time4 = datetime.strptime('13:46:11.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp1'] = (time3, time4)
+#
+# time5 = datetime.strptime('13:46:21.000000', '%H:%M:%S.%f').time()
+# time6 = datetime.strptime('13:46:59.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp2'] = (time5, time6)
+#
+# time7 = datetime.strptime('13:21:20.000000', '%H:%M:%S.%f').time()
+# time8 = datetime.strptime('13:33:01.000000', '%H:%M:%S.%f').time()
+# tag['boy2_longhaul'] = (time7, time8)
+
+
+# people = 'boy2'
+# time1 = datetime.strptime('14:21:43.000000', '%H:%M:%S.%f').time()
+# time2 = datetime.strptime('14:22:30.000000', '%H:%M:%S.%f').time()
+# tag['boy2_rpsk'] = (time1, time2)
+#
+# time3 = datetime.strptime('14:23:08.000000', '%H:%M:%S.%f').time()
+# time4 = datetime.strptime('14:23:43.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp1'] = (time3, time4)
+#
+# time5 = datetime.strptime('14:23:55.000000', '%H:%M:%S.%f').time()
+# time6 = datetime.strptime('14:24:23.000000', '%H:%M:%S.%f').time()
+# tag['boy2_situp2'] = (time5, time6)
+#
+# time7 = datetime.strptime('14:09:56.000000', '%H:%M:%S.%f').time()
+# time8 = datetime.strptime('14:21:35.000000', '%H:%M:%S.%f').time()
+# tag['boy2_longhaul'] = (time7, time8)
